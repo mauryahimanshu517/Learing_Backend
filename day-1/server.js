2effbd418cbeceef6b85430f71d89595c2b3e62d
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2026-02-01 21:31:22 +0530

    optimize code

--- day-1/server.js
+++ day-1/server.js
@@ -1,32 +1,4 @@
 import app from "./src/index.js"
-import express from "express"
-
-
-app.use(express.json())
-
-let notes=[]
-
-
-app.post("/createNotes",(req,res)=>{
-    res.send("notes created")
-    notes.push(req.body)
-    console.log(notes)
-})
-
-app.get("/getNotesData",(req,res)=>{
-    res.send(notes)
-})
-
-app.delete("/delete/:index",(req,res)=>{
- delete notes[req.params.index]
- res.send("deleted successfully")
-})
-
-app.patch("/update/:index",(req,res)=>{
-   notes[req.params.index].description = req.body.description
-    notes[req.params.index].title = req.body.title
-   res.send("list update successfully")
-})
 
 
 app.listen(5000,()=>{
--- day-1/src/index.js
+++ day-1/src/index.js
@@ -3,5 +3,33 @@ const express =require("express")
 const app=express()
 
 
+app.use(express.json())
+
+let notes=[]
+
+
+app.post("/createNotes",(req,res)=>{
+    res.send("notes created")
+    notes.push(req.body)
+    console.log(notes)
+})
+
+app.get("/getNotesData",(req,res)=>{
+    res.send(notes)
+})
+
+app.delete("/delete/:index",(req,res)=>{
+ delete notes[req.params.index]
+ res.send("deleted successfully")
+})
+
+app.patch("/update/:index",(req,res)=>{
+   notes[req.params.index].description = req.body.description
+    notes[req.params.index].title = req.body.title
+   res.send("list update successfully")
+})
+
+
+
 
 module.exports=app
