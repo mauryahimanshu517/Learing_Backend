d465e9242591e9d8e4797f390d409545504f6be3
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2026-02-05 21:54:36 +0530

    enhance file code

--- day-1/src/index.js
+++ day-1/src/index.js
@@ -2,31 +2,38 @@ const express =require("express")
 
 const app=express()
 
-
 app.use(express.json())
 
 let notes=[]
 
 
 app.post("/createNotes",(req,res)=>{
-    res.send("notes created")
+    res.status(201).json({
+        message:"notes created successfully"
+    })
     notes.push(req.body)
-    console.log(notes)
+   
 })
 
 app.get("/getNotesData",(req,res)=>{
-    res.send(notes)
+    res.status(200).json({
+        notes:notes
+    })
 })
 
 app.delete("/delete/:index",(req,res)=>{
  delete notes[req.params.index]
- res.send("deleted successfully")
+ res.status(204).json({
+    message:"notes deleted successfully"
+ })
 })
 
 app.patch("/update/:index",(req,res)=>{
    notes[req.params.index].description = req.body.description
     notes[req.params.index].title = req.body.title
-   res.send("list update successfully")
+   res.status(200).json({
+    message:"notes updated successfully"
+   })
 })
 
 
