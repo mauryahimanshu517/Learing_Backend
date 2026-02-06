9098c80b60bdef99a729b13351c9be0f79294622
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2026-02-06 15:01:03 +0530

    .env file should always be outside of src

--- day-4/server.js
+++ day-4/server.js
@@ -1,9 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import app from "./src/app.js"
 import connectToDB from "./src/config/dbConnect.js"
+
+
 
 connectToDB()
 
 
-app.listen(5000,(req,res)=>{
+app.listen(5000, (req, res) => {
     console.log("serevr is running on port 5000")
 })
--- day-4/src/config/dbConnect.js
+++ day-4/src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectToDB = async () => {
-  await mongoose.connect("mongodb+srv://mauryahimanshu517_db_user:eJ7nMbe5VGT4eggg@cluster0.9kxhpay.mongodb.net/day-4");
+  await mongoose.connect(`${process.env.DB_DATA}`);
   console.log("MongoDB connected");
 };
 
