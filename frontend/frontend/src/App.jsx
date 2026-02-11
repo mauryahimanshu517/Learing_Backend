e09eeb541af589f220916e2878d3eef492c65d60
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2026-02-11 23:28:25 +0530

    CRUD

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -4,29 +4,97 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [edit, setEdit] = useState("")
 
-  useEffect(() => {
+
+  function getData() {
     axios.get(
       "http://localhost:5000/api/getNotes"
     ).then((response) => {
       setData(response.data.getData)
     })
+  }
+
+  useEffect(() => {
+    getData()
 
   }, [])
 
+  function handleSumit(e) {
+    e.preventDefault()
+    const { title, description } = e.target.elements
+    console.log(title.value, description.value)
 
-  console.log(data)
+    axios.post("http://localhost:5000/api/createNotes", {
+      title: title.value,
+      description: description.value
+    }).then((res) => {
+      console.log(res)
+      getData()
+    })
+  }
+
+  function handleDelete(id) {
+    console.log("id", id)
+
+    axios.delete("http://localhost:5000/api/deteteNotes/" + id).then((res) => {
+      console.log(res)
+      getData()
+    })
+  }
+
+  function handleEdit(id, title, description) {
+    setEdit(id)
+    setTitle(title)
+    setDescription(description)
+  }
+
+
+  function handleUpdate() {
+    axios.patch("http://localhost:5000/api/updateNotes/" + edit, {
+      title,
+      description
+    }).then((res) => {
+      getData()
+      console.log(res)
+    })
+  }
+
+
 
   return (
     <>
-    {
-       data.map((items, index) => (
-        <div key={index}>
-          <p>Title: {items.title}</p>
-          <p>Description: {items.description}</p>
-        </div>
-      ))
-    }
+      <div>
+        <form onSubmit={handleSumit} >
+          <input type="text" value={title} name="title" placeholder="enter titile" onChange={(e) => setTitle(e.target.value)}></input>
+          <input type="text" value={description} name="description" placeholder="enter description" onChange={(e) => setDescription(e.target.value)}></input>
+          {edit ? (
+            <button type="button" onClick={handleUpdate}>
+              Update Value
+            </button>
+          ) : (
+            <button type="submit">
+              Create Notes
+            </button>
+          )}
+
+
+        </form>
+
+      </div>
+      {
+        data.map((items, index) => (
+          <div key={index}>
+            <p>Title: {items.title}</p>
+            <p>Description: {items.description}</p>
+            <button onClick={() => (handleDelete(items._id))}>delete</button>
+            <button onClick={() => (handleEdit(items._id, items.title, items.description))}>Update</button>
+          </div>
+
+        ))
+      }
     </>
   )
 
