f24427a03b3be5f641449ff0fc4009822d16a1d6
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2026-02-12 21:31:57 +0530

    done CRUD using frontend and backend

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -4,26 +4,25 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
   const [edit, setEdit] = useState("")
+  const [title, setTitle] = useState("")
+  const [description, setDecription] = useState("")
 
+  console.log(edit, title, description)
 
   function getData() {
-    axios.get(
-      "http://localhost:5000/api/getNotes"
-    ).then((response) => {
-      setData(response.data.getData)
+    axios.get("http://localhost:5000/api/getNotes").then((res) => {
+      setData(res.data.getData)
     })
   }
-
   useEffect(() => {
     getData()
-
   }, [])
+
 
   function handleSumit(e) {
     e.preventDefault()
+    console.log("helllo")
     const { title, description } = e.target.elements
     console.log(title.value, description.value)
 
@@ -33,32 +32,25 @@ function App() {
     }).then((res) => {
       console.log(res)
       getData()
+
     })
   }
 
   function handleDelete(id) {
-    console.log("id", id)
-
-    axios.delete("http://localhost:5000/api/deteteNotes/" + id).then((res) => {
+    axios.delete(`http://localhost:5000/api/deteteNotes/${id}`).then((res) => {
       console.log(res)
       getData()
     })
   }
 
-  function handleEdit(id, title, description) {
-    setEdit(id)
-    setTitle(title)
-    setDescription(description)
-  }
 
 
   function handleUpdate() {
-    axios.patch("http://localhost:5000/api/updateNotes/" + edit, {
-      title,
-      description
+    axios.patch(`http://localhost:5000/api/updateNotes/${edit}`, {
+      title, description
     }).then((res) => {
-      getData()
       console.log(res)
+      getData()
     })
   }
 
@@ -66,38 +58,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <form onSubmit={handleSumit} >
-          <input type="text" value={title} name="title" placeholder="enter titile" onChange={(e) => setTitle(e.target.value)}></input>
-          <input type="text" value={description} name="description" placeholder="enter description" onChange={(e) => setDescription(e.target.value)}></input>
-          {edit ? (
-            <button type="button" onClick={handleUpdate}>
-              Update Value
-            </button>
+      <form onSubmit={handleSumit}>
+        <input value={title} name="title" placeholder='enter title' onChange={(e) => (setTitle(e.target.value))}></input>
+        <input value={description} name="description" placeholder='enter description' onChange={(e) => (setDecription(e.target.value))}></input>
+        {
+          edit ? (
+            <>
+              <button type='button' onClick={handleUpdate}>update Edit</button>
+            </>
           ) : (
-            <button type="submit">
-              Create Notes
-            </button>
-          )}
+            <>
+              <button type='submit' >create Notes</button>
+            </>
+          )
+        }
 
 
-        </form>
+      </form>
 
+
+      <div>
+        <h1>hello</h1>
+        {
+          data?.map((items, index) => {
+            return <div key={index}>
+              <p>{items.title}</p>
+              <p>{items.description}</p>
+              <button onClick={() => (handleDelete(items._id))}>Delete</button>
+              <button onClick={() => (setEdit(items._id), setTitle(items.title), setDecription(items.description))}>Update</button>
+            </div>
+          })
+        }
       </div>
-      {
-        data.map((items, index) => (
-          <div key={index}>
-            <p>Title: {items.title}</p>
-            <p>Description: {items.description}</p>
-            <button onClick={() => (handleDelete(items._id))}>delete</button>
-            <button onClick={() => (handleEdit(items._id, items.title, items.description))}>Update</button>
-          </div>
-
-        ))
-      }
     </>
   )
-
 }
 
 export default App
