455c373e51d2b4cad1d1095e50844f65d58e2075
mauryahimanshu517 <mauryahimanshu517@gmail.com>
2026-03-02 16:07:27 +0530

    added hash for password

--- authPractice/src/routes/api.route.js
+++ authPractice/src/routes/api.route.js
@@ -1,43 +1,75 @@
 import dbModel from "../model/db.model.js"
 import express from "express"
 import cookieparser from "cookie-parser"
-
+import crypto from "crypto";
 import jwt from "jsonwebtoken"
 
 
-const authRouter=express.Router()
+const authRouter = express.Router()
 
 
-authRouter.post("/register", async(req,res)=>{
-    const {name,email,password}=req.body
-    
-    const isEmailAleardyExist=await dbModel.findOne({email})
-    if(isEmailAleardyExist){
+authRouter.post("/register", async (req, res) => {
+
+    const { name, email, password } = req.body
+
+    const isEmailAleardyExist = await dbModel.findOne({ email })
+
+    if (isEmailAleardyExist) {
         res.status(200).json({
-            message:"email already exist"
+            message: "email already exist"
         })
     }
-
-    const createPost=await dbModel.create({
-        name,email,password
+    const hash = crypto.createHash("md5").update(password).digest("hex");
+    const createPost = await dbModel.create({
+        name, email, password: hash
     })
 
-    const token=jwt.sign({
-    
-            id:createPost._id,
-            email:createPost.email
-        },
-        process.env.TOKEN_SECRET
-)
+    const token = jwt.sign({
 
-cookieparser(token)
+        id: createPost._id,
+        email: createPost.email
+    },
+        process.env.TOKEN_SECRET
+    )
+
+    cookieparser(token)
 
 
     res.status(201).json({
-        message:"user creatated",
+        message: "user creatated",
         createPost,
         token
 
+    })
+})
+
+authRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body
+
+    const isEmailExist = await dbModel.findOne({ email })
+
+    if (!isEmailExist) {
+        return res.status(404).json({
+            message: "email does not exist"
+        })
+    }
+
+    const isPasswordCorrect = isEmailExist.password ===  crypto.createHash("md5").update(password).digest("hex")
+
+    if (!isPasswordCorrect) {
+        return res.status(404).json({
+            message: "password does not exist"
+        })
+    }
+    const token = jwt.sign({
+        id: isEmailExist._id
+    }, process.env.TOKEN_SECRET)
+
+    res.cookie("jwt_token", token)
+
+    res.status(200).json({
+        message: "user logined successfully",
+        token
     })
 })
 
